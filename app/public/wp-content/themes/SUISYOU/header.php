fee61e852f890e479862fc83872ff566969f4a13
takashima-takuto <aku424tt@gmail.com>
2023-06-16 00:40:24 +0900

    feat : ハンバーガーメニュー設計

--- app/public/wp-content/themes/SUISYOU/header.php
+++ app/public/wp-content/themes/SUISYOU/header.php
@@ -9,9 +9,100 @@
     <?php wp_head(); ?>
 </head>
 <body>
-<header>
-    <div class="js-header">
-        ヘッダーです。
+<header class="m-header js-header">
+    <div class="m-header__inner">
+        <div class="m-header__layout">
+            <div class="m-header__logo">
+                <img src="<?php echo get_template_directory_uri()?>/common/img/icon_common_08.svg" alt="翠翔">
+            </div><!-- /.m-header__logo -->
+            <div class="m-header__main">
+                <button class="m-header__button js-header__hook" type="button">
+                    <span class="m-header__buttonText">MENU</span>
+                </button>
+                <div class="m-header__contents js-header__navContents">
+                    <div class="m-header__contentsInner">
+                        <div class="m-header__media">
+                            <img src="<?php echo get_template_directory_uri()?>/img/img_index_23.png" alt="">
+                        </div><!-- /.m-header__media -->
+                        <div class="m-header__info">
+                            <div class="m-header__infoInner">
+                                <div class="m-header__infoLogo">
+                                    <img src="<?php echo get_template_directory_uri()?>/common/img/icon_common_08.svg" alt="翠翔">
+                                </div><!-- /.m-header__infoLogo -->
+                                <ul class="m-header__list">
+                                    <li class="m-header__link">
+                                        <a href="__dummy__">
+                                            <img src="<?php echo get_template_directory_uri()?>/common/img/icon_common_12.svg" alt="Instagram">
+                                        </a>
+                                    </li>
+
+                                    <li class="m-header__link">
+                                        <a href="__dummy__">
+                                            <img src="<?php echo get_template_directory_uri()?>/common/img/icon_common_03.svg" alt="LINE">
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div><!-- /.m-header__infoInner -->
+                            <nav class="m-header__nav">
+                                <ul class="m-header__navList">
+                                    <div class="m-header__navRight">
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">メニュー一覧</span>
+                                                <span class="m-header__navSubText">MENU</span>
+                                            </a>
+                                        </li>
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">コースについて</span>
+                                                <span class="m-header__navSubText">CORSE</span>
+                                            </a>
+                                        </li>
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">店舗案内</span>
+                                                <span class="m-header__navSubText">SHOP</span>
+                                            </a>
+                                        </li>
+
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">お知らせ</span>
+                                                <span class="m-header__navSubText">NEWS</span>
+                                            </a>
+                                        </li>
+                                    </div><!-- /.m-header__navRight -->
+
+                                    <div class="m-header__navLeft">
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">お問い合わせ</span>
+                                                <span class="m-header__navSubText">CONTACT</span>
+                                            </a>
+                                        </li>
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">ONLINE SHOP
+                                                    <img class="m-newWindowIcon" src="<?php echo get_template_directory_uri()?>/common/img/icon_common_11.svg" alt="別窓ウィンドウで開く">
+                                                </span>
+                                            </a>
+                                        </li>
+                                        <li class="m-header__navItem">
+                                            <a href="" class="m-header__navLink">
+                                                <span class="m-header__navText">TAKEOUT弁当
+                                                    <img class="m-newWindowIcon" src="<?php echo get_template_directory_uri()?>/common/img/icon_common_11.svg" alt="別窓ウィンドウで開く">
+                                                </span>
+                                            </a>
+                                        </li>
+                                    </div><!-- /.m-header__navLeft -->
+                                </ul>
+                            </nav>
+                        </div><!-- /.m-header__info -->
+                    </div><!-- /.m-header__contentsInner -->
+                </div><!-- /.m-header__contents -->
+            </div><!-- /.m-header__main -->
+        </div><!-- /.m-header__layout -->
     </div>
-    <!-- /.js-header -->
+    <!-- /.m-header__inner -->
 </header>
+<!-- /.header -->
--- src/dev/scripts/modules/header.js
+++ src/dev/scripts/modules/header.js
@@ -4,12 +4,40 @@ export default class Header {
      */ 
     constructor(root) {
         this.root = root;
+        this.hook = this.root.querySelector('.js-header__hook');
+        this.contents = this.root.querySelector('.js-header__navContents');
+
+        // class
+        this.activeClass = 'is-active';
+
+        // flag
+        this.isOpenFlag = false;
     };
 
     /**
      * 初期発火 
      */ 
     init() {
-        console.log(this.root);
+        this.setWaiAria();
+        this.clickBind();
     };
+
+    setWaiAria() {
+        console.log(this.hook);
+        this.hook.setAttribute('aria-expand', 'false');
+        this.hook.setAttribute('aria-controls', 'hamburgerMenu');
+        this.contents.setAttribute('id', 'hamburgerMenu');
+    }
+
+    /**
+     * ハンバーガーメニューのボタンがクリックされた時のバインド処理 
+     */
+    clickBind() {
+        this.hook.addEventListener('click', () => {
+            console.log(this.hook);
+            this.isOpenFlag = !this.isOpenFlag;
+            this.root.classList.toggle(this.activeClass);
+            this.hook.setAttribute('aria-expand', `'${this.isOpenFlag}'`);
+        });
+    }
 };
